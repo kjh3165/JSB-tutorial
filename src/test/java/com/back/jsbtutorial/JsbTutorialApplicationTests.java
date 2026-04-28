707bc5d3f3e7b81e7b56ae57fbf1c83218744a0a
kjh3165 <jhwk109@gmail.com>
2026-04-28 09:52:48 +0900

    프로젝트 세팅

---
+++ src/test/java/com/back/jsbtutorial/JsbTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.jsbtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsbTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
