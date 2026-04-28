2af41214b2c036dae65d934d9e5aff2f7668842a
kjh3165 <jhwk109@gmail.com>
2026-04-28 11:28:05 +0900

    h2 추가, dev와 test 프로파일 분리

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testAnnotationProcessor("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-h2console")
 }
 
 tasks.withType<Test> {
