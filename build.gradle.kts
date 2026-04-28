b9d7cdff73f3059115ee903070c128dbe6e5128b
kjh3165 <jhwk109@gmail.com>
2026-04-28 11:30:04 +0900

    JPA 추가, SQL 로그 출력 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     testAnnotationProcessor("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-h2console")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.withType<Test> {
