dcf6f34fae60eb6e871f9255cf4ead7ce38baaaa
kjh3165 <jhwk109@gmail.com>
2026-04-29 12:31:20 +0900

    타임리프 도입, 컨트롤러가 아니라 question_list.html에서 응답내용을 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-h2console")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation ("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
 }
 
 tasks.withType<Test> {
