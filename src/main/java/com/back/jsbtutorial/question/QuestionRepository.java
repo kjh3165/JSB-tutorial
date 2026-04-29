b1d3c48ccdb57ea35824865f23e6630bf9a367e8
kjh3165 <jhwk109@gmail.com>
2026-04-29 14:35:08 +0900

    서비스 활용하기

--- src/main/java/com/back/jsbtutorial/question/QuestionRepository.java
+++ src/main/java/com/back/jsbtutorial/question/QuestionRepository.java
@@ -1,10 +1,12 @@
 package com.back.jsbtutorial.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Optional<Question> findBySubject(String subject);
     Optional<Question> findBySubjectAndContent(String subject, String content);
