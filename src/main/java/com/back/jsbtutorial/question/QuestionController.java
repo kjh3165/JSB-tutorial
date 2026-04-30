907cd050ce2241b0c77a53a98e48c83d3b131e00
kjh3165 <jhwk109@gmail.com>
2026-04-30 12:23:08 +0900

    질문 등록 벨리데이션 처리

--- src/main/java/com/back/jsbtutorial/question/QuestionController.java
+++ src/main/java/com/back/jsbtutorial/question/QuestionController.java
@@ -1,9 +1,14 @@
 package com.back.jsbtutorial.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -34,8 +39,12 @@ public class QuestionController {
     }
 
     @PostMapping("/create")
-    public String create(@RequestParam("subject") String subject, @RequestParam("content") String content){
-        questionService.create(subject, content);
+    public String create(@Valid QuestionForm questionForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
     }
 }
